0ef604a43a6ce86601cbc62cef76e6be3298c663
Usama Mahmood <139591974+MrUsama07@users.noreply.github.com>
2024-08-08 02:39:34 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://zolavila.buzz//` + encodeURI(path as string)
+					`https:/zolavila.buzz/` + encodeURI(path as string)
 				}`,
 			},
 		};
